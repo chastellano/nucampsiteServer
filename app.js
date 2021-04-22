0961c598b5c0a9559688285543e88077754cf62d
Chastellano <charles.a.castellano@gmail.com>
2021-04-22 14:09:59 -0400

    passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -46,22 +48,19 @@ app.use(session({
   store: new FileStore()
 }))
 
-function auth(req, res, next) {
-  console.log(req.session);
+app.use(passport.initialize());
+app.use(passport.session());
 
-  if (!req.session.user) {
-    const err = new Error('You are not authenticated');
-    err.status = 401;
-    return next(err);
-  } else {
-    if (req.session.user === 'authenticated') {
-      return next();
+function auth(req, res, next) {
+    console.log(req.user);
+
+    if (!req.user) {
+        const err = new Error('You are not authenticated');
+        err.status = 401;
+        return next(err);
     } else {
-      const err = new Error('You are not authenticated');
-      err.status = 401;
-      return next(err);
+        return next();
     }
-  }
 }
 
 app.use('/users', usersRouter);
