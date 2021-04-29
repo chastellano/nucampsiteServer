8f14be742dcd95d92d8fa3558520e863f975cc1e
Chastellano <charles.a.castellano@gmail.com>
2021-04-29 18:52:51 -0400

    HTTPS

--- authenticate.js
+++ authenticate.js
@@ -37,4 +37,16 @@ exports.jwtPassport = passport.use(
     )
 );
 
+
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+exports.verifyAdmin = (req, res, next) => {
+    // console.log(req.user.admin);
+    if (!req.user.admin) {
+        const err = new Error('You are not authorized to perform this operation!');
+        err.statusCode = 403;
+        return next(err);
+    } else {
+        return next();
+    }
+}
