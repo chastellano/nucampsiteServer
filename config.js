aede93172f685ab21bb3202f97df6fa1f0ff3f4e
Chastellano <charles.a.castellano@gmail.com>
2021-05-01 00:31:28 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '574020750657239',
+        clientSecret: '79ddb9c4e9a0b61bdc96d98fb905d7c3'
+    }
 }
